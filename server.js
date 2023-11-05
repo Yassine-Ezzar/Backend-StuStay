b685b22332ba55af3fe67fda20ea3bccb4bcf581
ons <ons.bouhjar@esprit.tn>
2023-11-05 22:15:47 +1200

    CRUD

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const logementRoutes = require('./routes/logement');
+import express from 'express';
+import mongoose from 'mongoose';
+import reclamationRoutes from './routes/reclamation.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connexion à la base de données (à personnaliser)
-mongoose.connect('mongodb://localhost/stustay_db', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Gérer les erreurs de connexion à la base de données
-mongoose.connection.on('error', (err) => {
-  console.error(`Erreur de connexion à la base de données : ${err}`);
-});
+mongoose.set("debug", true)
+mongoose.Promise = global.Promise;
 
-// Middleware pour analyser le corps des requêtes en JSON
-app.use(bodyParser.json());
 
-// Utiliser les routes pour la gestion des logements
-app.use('/logements', logementRoutes);
+const databaseName = 'stustay-db';
+
+// Cela afichera les requêtes MongoDB dans le terminal
+mongoose.set('debug', true);
+// Utilisation des promesses ES6 pour Mongoose, donc aucune callback n'est nécessaire
+mongoose.Promise = global.Promise;
+
+// Se connecter à MongoDB
+mongoose
+  .connect(`mongodb://localhost:27017/${databaseName}`)
+  .then(() => {
+    // Une fois connecté, afficher un message de réussite sur la console
+    console.log(`Connected to ${databaseName}`);
+  })
+  .catch(err => {
+    // Si quelque chose ne va pas, afficher l'erreur sur la console
+    console.log(err);
+  });
+
+
+
+app.use(express.json());
+app.use('/reclamations', reclamationRoutes);
 
 // Gestion des erreurs
 app.use((err, req, res, next) => {
