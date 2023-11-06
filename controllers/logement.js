6f385d31e8612fe92744962c7bdce0f8f215c9c0
Yassine Ezzar <yassine.ezzar@esprit.tn>
2023-11-06 21:08:09 +0100

    crud

--- controllers/logement.js
+++ controllers/logement.js
@@ -1,63 +1,62 @@
+// controllers/logementController.js
 const Logement = require('../models/logement');
 
-// CREATE - Créer un nouveau logement
+// Créer un nouveau logement
 exports.createLogement = async (req, res) => {
   try {
     const logement = new Logement(req.body);
-    await logement.save();
-    res.status(201).json(logement);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    const savedLogement = await logement.save();
+    res.status(201).json(savedLogement);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la création du logement' });
   }
 };
 
-// READ - Obtenir la liste des logements
+// Obtenir la liste des logements
 exports.getAllLogements = async (req, res) => {
   try {
     const logements = await Logement.find();
     res.json(logements);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération de la liste des logements' });
   }
 };
 
-// READ - Obtenir un logement par son ID
+// Obtenir un logement par ID
 exports.getLogementById = async (req, res) => {
   try {
     const logement = await Logement.findById(req.params.id);
     if (!logement) {
-      return res.status(404).json({ message: 'Logement introuvable' });
+      return res.status(404).json({ message: 'Logement non trouvé' });
     }
     res.json(logement);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération du logement' });
   }
 };
 
-// UPDATE - Mettre à jour un logement par son ID
+// Mettre à jour un logement par ID
 exports.updateLogement = async (req, res) => {
   try {
-    const logement = await Logement.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!logement) {
-      return res.status(404).json({ message: 'Logement introuvable' });
+    const updatedLogement = await Logement.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedLogement) {
+      return res.status(404).json({ message: 'Logement non trouvé' });
     }
-    res.json(logement);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.json(updatedLogement);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la mise à jour du logement' });
   }
 };
 
-// DELETE - Supprimer un logement par son ID
+// Supprimer un logement par ID
 exports.deleteLogement = async (req, res) => {
   try {
-    const logement = await Logement.findByIdAndRemove(req.params.id);
-    if (!logement) {
-      return res.status(404).json({ message: 'Logement introuvable' });
+    const deletedLogement = await Logement.findByIdAndRemove(req.params.id);
+    if (!deletedLogement) {
+      return res.status(404).json({ message: 'Logement non trouvé' });
     }
-    res.json({ message: 'Logement supprimé avec succès' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.json({ message: 'Logement supprimé' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la suppression du logement' });
   }
 };
--- models/logement.js
+++ models/logement.js
@@ -1,14 +1,15 @@
-// models/Logement.js
+// models/logement.js
 const mongoose = require('mongoose');
 
 const logementSchema = new mongoose.Schema({
-  image: String,
+  images: [String], 
   titre: String,
   description: String,
   nom: String,
-  lieu: String,
   nombreChambre: Number,
   prix: Number,
+  contact: String,
+  lieu: String, 
 });
 
 module.exports = mongoose.model('Logement', logementSchema);
