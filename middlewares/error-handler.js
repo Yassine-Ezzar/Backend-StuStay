7fca82204b9da5d648b40c5e39219652b9927d45
Yassine Ezzar <yassine.ezzar@esprit.tn>
2023-11-06 19:42:38 +0100

    FIRST COMMIT

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,12 +1,18 @@
-// middlewares/error-handler.js
+export function errorHandler(err, req, res, next) {
+  console.error(`Erreur survenue : ${err.message}`);
 
-function errorHandler(err, req, res, next) {
-    console.error(err); // Vous pouvez effectuer des opérations de journalisation avancées ici
-  
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Erreur interne du serveur';
-  
-    res.status(statusCode).json({ error: message });
+  if (err.name === 'ValidationError') {
+      res.status(400).json({ error: err.message, errors: err.errors });
+  } else if (err.name === 'CastError' && err.kind === 'ObjectId') {
+      res.status(404).json({ error: 'ID invalide' });
+  } else if (err.name === 'MongoError' && err.code === 11000) {
+      res.status(409).json({ error: 'Erreur de clé en double', champ: err.keyValue });
+  } else {
+      res.status(500).json({ error: 'Erreur interne du serveur' });
   }
-  
-  module.exports = errorHandler;
+}
+
+export function notFoundError(req, res, next) {
+  console.error(`Ressource non trouvée : ${req.originalUrl}`);
+  res.status(404).json({ error: `Ressource non trouvée - ${req.originalUrl}` });
+}
